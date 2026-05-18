f5b36cfd7fe0fe6cff34f19ca719880d36642735
Luis Valentin Ortiz <luis77vno@gmail.com>
2026-05-18 12:16:01 -0300

    aura farming

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,4 +1,7 @@
 public class Grupo {
     private String identificacion;
     private String descripcion;
+    public int obtenerPuntos(Seleccion s){
+        return 0;
+    }
 }
--- src/Participacion.java
+++ src/Participacion.java
@@ -1,3 +1,12 @@
 public class Participacion {
     private boolean esLocal;
+    public int cantidadGoles(){
+        return 0;
+    }
+    public int cantidadTarjAmarillas(){
+        return 0;
+    }
+    public int cantidadTarjRojas(){
+        return 0;
+    }
 }
