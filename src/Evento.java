05a54b8aa1cbc4f89bf0162ef175d66e744e76dd
Luis Valentin Ortiz <luis77vno@gmail.com>
2026-06-17 10:04:48 -0300

    pequeños cambios en atributos

--- src/Evento.java
+++ src/Evento.java
@@ -1,42 +1,26 @@
 public class Evento {
     private TipoEvento tipo;
     private int minuto;
-    private Jugador jugador;
+    private Jugador jugador; // CORRECCIÓN: El jugador involucrado ahora es atributo obligatorio
 
-    // Constructores
-
+    // 1. Constructor vacío
     public Evento() {
     }
 
-    public Evento(TipoEvento tipo, int minuto) {
+    // 2. Constructor parametrizado (Recibe TODO lo que involucra)
+    public Evento(TipoEvento tipo, int minuto, Jugador jugador) {
         this.tipo = tipo;
         this.minuto = minuto;
+        this.jugador = jugador;
     }
 
     // Getters y Setters
+    public TipoEvento getTipo() { return tipo; }
+    public void setTipo(TipoEvento tipo) { this.tipo = tipo; }
 
-    public TipoEvento getTipo() {
-        return tipo;
-    }
+    public int getMinuto() { return minuto; }
+    public void setMinuto(int minuto) { this.minuto = minuto; }
 
-    public void setTipo(TipoEvento tipo) {
-        this.tipo = tipo;
-    }
-
-    public int getMinuto() {
-        return minuto;
-    }
-
-    public void setMinuto(int minuto) {
-        this.minuto = minuto;
-    }
-
-    public Jugador getJugador() {
-        return jugador;
-    }
-
-    // Único setter puro para evitar la recursión (bucle infinito)
-    public void setJugador(Jugador jugador) {
-        this.jugador = jugador;
-    }
+    public Jugador getJugador() { return jugador; }
+    public void setJugador(Jugador jugador) { this.jugador = jugador; }
 }
